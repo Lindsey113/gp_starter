5a4d8c74b5221220128d3090d369abb14af283d0
Lindsey McCluskey <lindsey_mccluskey@yahoo.ca>
2024-06-29 18:00:42 -0700

    done

--- frontend/hooks/index.js
+++ frontend/hooks/index.js
@@ -3,10 +3,34 @@ import axios from 'axios'
 
 const BASE_URL = 'http://localhost:9009/acme/auth'
 
-export function useAuth(redirect) {
 
+export function useAuth(redirect) {
+    const [auth, setAuth] = useState(null)
+    async function checkAuth() {
+        try {
+          await axios.get(
+            `${BASE_URL}/is_authed`,
+            { headers: { Authorization: localStorage.getItem('token') } },
+          )
+          setAuth(true)
+        } catch (err) {
+          localStorage.removeItem('token')
+          setAuth(false)
+        }
+    }
+    useEffect(() => {
+        if (!localStorage.getItem('token')) redirect()
+        else checkAuth()
+      }, [])
+      useEffect(() => {
+        if (auth === false) redirect()
+      }, [auth])
+  
+  return {auth, checkAuth}
 }
 
 export function useInput(name, initialValue = '') {
-
+    const [value, setValue] = useState(initialValue)
+    const onChange = e => setValue(e.target.value)
+    return {value, onChange, name}
 }
